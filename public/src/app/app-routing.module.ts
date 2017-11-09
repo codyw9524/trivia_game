ceecc218d080433b0811f053f9397efe97dfb475
Cody Williams <codyw9524@gmail.com>
2017-11-09 09:46:25 -0600

    Update app-routing.module.ts

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { ApiDashboardComponent } from './api-dashboard/api-dashboard.component';
-// import { NewQuestionComponent } from './trivia/new-question/new-question.component';
-// import { DashboardComponent } from './trivia/dashboard/dashboard.component';
-// import { TriviaComponent } from './trivia/trivia.component';
-// import { LoginComponent } from './login/login.component';
-// import { GameComponent } from './trivia/game/game.component';
-// import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
     {
